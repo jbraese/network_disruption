184f3ca364041ac9603dc97e5e599b567d1d3061
Johannes <braese.johannes@gmail.com>
2020-01-01 14:03:04 +0100

    added file that sets up anaconda env for analysis

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,11 +1,37 @@
+# this script sets up an anaconda setup_environment
+# for network analysis using a few python analyis
 
 
-### initial set up of linux server
+# download anaconda
+#(update link with newest version from anaconda.com/distribution)
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 
-# generate new user with sudo privs
-# enable firewall and allow ssh
+# execute
+bash Anaconda3-2019.10-Linux-x86_64.sh
+# agree to licencse and accept default location
+# agree to prepend conda to PATH
 
+#activate installation
+source ~/.bashrc
 
-### set up git
+# set up and activate new python 3.6 anaconda environment
+conda create --name network_manila python=3.6
+conda activate network_manila
 
-### set up anaconda
+#change to project folder
+cd /home/op/network_manila
+
+conda install jupyter notebook
+
+conda install -c conda-forge rtree geopandas rasterio geojson
+conda install -c conda-forge gdal
+pip install geopy
+pip install boltons
+pip install pulp
+
+git clone https://github.com/worldbank/GOSTnets.git
+python setup.py build
+python setup.py install
+
+#further envs?
